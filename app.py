dd728a79c76b28f1a4724df5a46f0906b2ae0f4a
Richard Vankoningsveld <richyvk@gmail.com>
2017-02-18 19:56:40 +1000

    Add index and add movie routes

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+import records
+
+app = Flask(__name__)
+
+DB = 'sqlite:///database.db'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/movie/', methods=['GET', 'POST'])
+def movie():
+    if request.method == 'POST':
+
+        m_title = request.form.get('title')
+        m_year = request.form.get('year')
+        m_genre = request.form.get('genre')
+        m_description = request.form.get('description')
+        m_rating = request.form.get('rating')
+        print(m_title, m_year, m_genre, m_description, m_rating)
+
+        try:
+            print("Adding record")
+            db = records.Database(DB)
+            print("Connected to DB")
+            query = ('INSERT INTO movies (title, year, genre, '
+                     'description, rating) VALUES(:title, :year, '
+                     ':genre, :description, :rating)')
+            print(query)
+            db.query(query, title=m_title, year=m_year, genre=m_genre,
+                     description=m_description, rating=m_rating)
+            print("Success: record added")
+        except:
+            db.rollback()
+            print("Error adding record")
+        finally:
+            return render_template('index.html')
+    else:
+        return render_template('add-movie.html')
